5b7f7d9e4d9fcec28e108665f1eddc4898faa038
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-12-27 17:16:30 -0800

    Added SnakeModel constructor
    Added the SnakeModel constructor and tested it.
    All it does is set up the map and the player field. It also puts a SnakeTile where the player's position is defined.

--- snakeModel/snakeModel.cpp
+++ snakeModel/snakeModel.cpp
@@ -10,6 +10,10 @@ bool Snake::operator==(const Snake& other) const {
         && this->positionY == other.positionY;
 }
 
+SnakeModel::SnakeModel(int mapWidth, int mapHeight, int playerStartX, int playerStartY, Direction playerStartDirection): 
+  map(std::vector<std::vector<MapTileState>>(mapWidth,std::vector<MapTileState>(mapHeight,Empty))), player(playerStartDirection, playerStartX, playerStartY) {
+    this->map[playerStartX][playerStartY] = SnakeTile;
+}
 
 std::vector<std::vector<MapTileState>> SnakeModel::getMap() {
     return this->map;
--- test/snakeModelTest.cpp
+++ test/snakeModelTest.cpp
@@ -14,3 +14,25 @@ TEST(SnakeModelTest,SnakeConstructorTest){
     EXPECT_EQ(test.positionY,expectedPositionY);
 }
 
+TEST(SnakeModelTest,SnakeModelConstructorTest){
+    int mapWidth = 8;
+    int mapHeight = 8;
+    int playerStartingPositionX = 3;
+    int playerStartingPositionY = 4;
+    Direction startingDirection = Right;
+    SnakeModel test(mapWidth, mapHeight, playerStartingPositionX, playerStartingPositionY, startingDirection);
+
+    Snake expectedSnake(startingDirection,playerStartingPositionX,playerStartingPositionY);
+    std::vector<std::vector<MapTileState>> expectedMap{
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,SnakeTile,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        std::vector<MapTileState>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+    };
+    EXPECT_EQ(test.getPlayer(),expectedSnake);
+    EXPECT_EQ(test.getMap(),expectedMap);
+}
