79d92d0a9466c457baa62713c8e4644446b5ff61
StickonFire <70039286+StickonFire@users.noreply.github.com>
2025-12-30 15:50:59 -0800

    Added skeleton test for AdvanceState for Boundary testing
    Added a skeleton test for the AdvanceState to Boundary Test. This is the SnakeModelAdvanceStateTest::SingleTileTest. It merely cycles through every possible Direction-Command combination and checks if it correctly handles hitting a boundary, or, in the case of Pause, stays in place.

--- test/snakeModelTest.cpp
+++ test/snakeModelTest.cpp
@@ -1,6 +1,9 @@
+#include <array>
 #include <gtest/gtest.h>
 
 #include "snakeModel.hpp"
+
+using testing::Test;
 
 TEST(SnakeModelTest,SnakeConstructorTest){
     Direction expectedDirection = Left;
@@ -52,4 +55,42 @@ TEST(SnakeModelTest,ChangeDirectionTest){
     EXPECT_EQ(changeDirection(Down,LeftTurn),Right);
     EXPECT_EQ(changeDirection(Down,Straight),Down);
     EXPECT_EQ(changeDirection(Down,RightTurn),Left);
+}
+
+const std::array<Command,3> commandList{LeftTurn,RightTurn,Pause};
+const std::array<Direction,4> directionList{Up,Right,Down,Left};
+
+class SnakeModelAdvanceStateTest : public testing::Test {
+
+    protected:
+    SnakeModel testModel;
+
+    SnakeModelAdvanceStateTest(): testModel(1,1,Coordinate(0,0),Up) { }
+
+    void checkKilled(){
+
+    }
+
+    void singleTileTest(Direction startDirection){
+        for(Command input: commandList){
+            testModel = SnakeModel(1,1,Coordinate(0,0),startDirection);
+            testModel.advanceState(input);
+            if(input == Pause){
+                SnakeModel expectedModel(1,1,Coordinate(0,0),startDirection);
+                expectedModel.togglePause();
+                EXPECT_EQ(expectedModel,testModel);
+                EXPECT_TRUE(testModel.isPaused());
+            } else {
+                checkKilled();
+            }
+        }
+    }
+
+};
+
+TEST_F(SnakeModelAdvanceStateTest,SingleTileTest){
+    for(Direction testDirection: directionList){
+        singleTileTest(testDirection);
+    }
+}
 }
