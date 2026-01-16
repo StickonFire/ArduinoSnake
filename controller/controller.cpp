9702a31aaf5f574b697dd2044e8af513b39fbefb
StickonFire <70039286+StickonFire@users.noreply.github.com>
2026-01-15 19:32:55 -0800

    Implemented ConstrainedVectorToArray::convertConstrainedVector
    Implemented the function ConstrainedVectorToArray::convertConstrainedVector.
    
    It places the input's values into the middle of the bool output array, with some bias up and left if the screen size is odd.

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -4,6 +4,22 @@
 #include "hardwareValues.hpp"
 #include "modelControllerCommUnits.hpp"
 #include "controller.hpp"
+
+void ConstrainedVectorToArray::convertConstrainedVector(ConstrainedVector<ConstrainedVector<MapTileState,ledCols>,ledRows> &input, bool output[ledRows][ledCols]){
+    int screenMaxX = (ledRows+input.size())/2;
+    int screenMinX = screenMaxX-input.size();
+    int screenMaxY = (ledCols+input[0].size())/2;
+    int screenMinY = screenMaxY-input[0].size();
+    for(int i = 0; i < ledRows; i++){
+        for(int j = 0; j < ledCols; j++){
+            if(i >= screenMinX && i < screenMaxX && j >= screenMinY && j < screenMaxY)
+                output[i][j] = ((input[i-screenMinX][j-screenMinY] == Empty) ? false : true);
+            else
+                output[i][j] = true;
+        }
+    }
+}
+
 Command ControlsParser::parseButtons(std::array<bool,3> &inputs){
     if(inputs[1])
         return Pause;
