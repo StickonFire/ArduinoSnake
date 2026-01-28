edf9fc583d18fa6ecf7f504afe618ac3501f8167
StickonFire <70039286+StickonFire@users.noreply.github.com>
2026-01-27 21:29:31 -0800

    Moved SnakeModelConstructorTest and ChangeDirectionTest
    Moved these two tests to after the snakeModelToString function, since SnakeModelConstructorTest now has a call to that function. ChangeDirectionTest is moved so that snakeModelToString is on top.

--- SnakeArduinoPlatformIO/test/test_software/snakeModelTest.cpp
+++ SnakeArduinoPlatformIO/test/test_software/snakeModelTest.cpp
@@ -7,48 +7,6 @@
 
 using testing::Test;
 
-TEST(SnakeModelTest,SnakeModelConstructorTest){
-    int mapWidth = 8;
-    int mapHeight = 8;
-    Coordinate playerStartingPosition(3,4);
-    Direction startingDirection = Right;
-    bool expectedPause = false;
-    SnakeModel test(mapWidth, mapHeight, playerStartingPosition, startingDirection);
-
-    Snake expectedSnake(startingDirection,playerStartingPosition);
-    ConstrainedVector<ConstrainedVector<MapTileState,ledCols>,ledRows> expectedMap{
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,SnakeTile,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
-    };
-    EXPECT_EQ(test.getPlayer(),expectedSnake);
-    EXPECT_EQ(test.getMap(),expectedMap);
-    EXPECT_EQ(test.isPaused(),expectedPause);
-
-    SnakeModel testFullConstructor(expectedMap,playerStartingPosition,startingDirection,expectedPause);
-    EXPECT_EQ(test,testFullConstructor);
-}
-
-TEST(SnakeModelTest,ChangeDirectionTest){
-    EXPECT_EQ(changeDirection(Left,LeftTurn),Down);
-    EXPECT_EQ(changeDirection(Left,Straight),Left);
-    EXPECT_EQ(changeDirection(Left,RightTurn),Up);
-    EXPECT_EQ(changeDirection(Up,LeftTurn),Left);
-    EXPECT_EQ(changeDirection(Up,Straight),Up);
-    EXPECT_EQ(changeDirection(Up,RightTurn),Right);
-    EXPECT_EQ(changeDirection(Right,LeftTurn),Up);
-    EXPECT_EQ(changeDirection(Right,Straight),Right);
-    EXPECT_EQ(changeDirection(Right,RightTurn),Down);
-    EXPECT_EQ(changeDirection(Down,LeftTurn),Right);
-    EXPECT_EQ(changeDirection(Down,Straight),Down);
-    EXPECT_EQ(changeDirection(Down,RightTurn),Left);
-}
-
 const std::array<Command,3> commandList{LeftTurn,RightTurn,Pause};
 const std::array<Direction,4> directionList{Up,Right,Down,Left};
 
@@ -105,6 +63,48 @@ std::string snakeModelToString(SnakeModel& toDescribe){
     else 
         result += "False\n";
     return result + "}";
+}
+
+TEST(SnakeModelTest,SnakeModelConstructorTest){
+    int mapWidth = 8;
+    int mapHeight = 8;
+    Coordinate playerStartingPosition(3,4);
+    Direction startingDirection = Right;
+    bool expectedPause = false;
+    SnakeModel test(mapWidth, mapHeight, playerStartingPosition, startingDirection);
+
+    Snake expectedSnake(startingDirection,playerStartingPosition);
+    ConstrainedVector<ConstrainedVector<MapTileState,ledCols>,ledRows> expectedMap{
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,SnakeTile,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+        ConstrainedVector<MapTileState,ledCols>{Empty,Empty,Empty,Empty,Empty,Empty,Empty,Empty},
+    };
+    EXPECT_EQ(test.getPlayer(),expectedSnake);
+    EXPECT_EQ(test.getMap(),expectedMap);
+    EXPECT_EQ(test.isPaused(),expectedPause);
+
+    SnakeModel testFullConstructor(expectedMap,playerStartingPosition,startingDirection,expectedPause);
+    EXPECT_EQ(test,testFullConstructor) << "Received: " << snakeModelToString(test) << "\nExpected: " << snakeModelToString(testFullConstructor);
+}
+
+TEST(SnakeModelTest,ChangeDirectionTest){
+    EXPECT_EQ(changeDirection(Left,LeftTurn),Down);
+    EXPECT_EQ(changeDirection(Left,Straight),Left);
+    EXPECT_EQ(changeDirection(Left,RightTurn),Up);
+    EXPECT_EQ(changeDirection(Up,LeftTurn),Left);
+    EXPECT_EQ(changeDirection(Up,Straight),Up);
+    EXPECT_EQ(changeDirection(Up,RightTurn),Right);
+    EXPECT_EQ(changeDirection(Right,LeftTurn),Up);
+    EXPECT_EQ(changeDirection(Right,Straight),Right);
+    EXPECT_EQ(changeDirection(Right,RightTurn),Down);
+    EXPECT_EQ(changeDirection(Down,LeftTurn),Right);
+    EXPECT_EQ(changeDirection(Down,Straight),Down);
+    EXPECT_EQ(changeDirection(Down,RightTurn),Left);
 }
 
 class SnakeModelAdvanceStateTest : public testing::Test {
