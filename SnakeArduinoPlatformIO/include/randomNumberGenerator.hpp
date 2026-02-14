d807bc33027f51b6255c109feeb8ac97629970f2
StickonFire <70039286+StickonFire@users.noreply.github.com>
2026-02-13 18:58:51 -0800

    Made raundomNumberGenerator functions public

--- SnakeArduinoPlatformIO/include/randomNumberGenerator.hpp
+++ SnakeArduinoPlatformIO/include/randomNumberGenerator.hpp
@@ -3,12 +3,14 @@
 #include <random>
 
 class randomNumberGenerator {
+    public:
     virtual int selectNumber(int maximumNumber) = 0;
 };
 
 class mt19937RandomNumberGenerator : randomNumberGenerator {
     std::mt19937 rng;
 
+    public:
     mt19937RandomNumberGenerator(std::mt19937 rng);
     int selectNumber(int maximumNumber);
 };
